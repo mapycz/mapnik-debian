16c0bff94ccdf72062b20f0a1e1c058b37174e70
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2019-05-23 10:19:02 +0200

    Force rebuild

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t mapnik-jessie -f jessie.Dockerfile .
-docker build -t mapnik-stretch -f stretch.Dockerfile .
+docker build --no-cache -t mapnik-jessie -f jessie.Dockerfile .
+docker build --no-cache -t mapnik-stretch -f stretch.Dockerfile .
